26e7f9d3ba5c87e72d6134102fda98918388bea4
Marcelo <83785793+Celo17@users.noreply.github.com>
2023-10-09 21:37:00 +0000

    atv função arrow

---
+++ atvFunctions.js
@@ -0,0 +1,11 @@
+function semParametro() {
+
+}
+
+function comParametro (nome) {
+    console.log (`olá ${nome}`);
+}
+comParametro ("marcelo")
+
+const nome = saudacao => `olá ${nome}`
+console.log (nome, "Manoela")
